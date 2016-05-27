ec69c47f37fce47d9f91fd73e19029da352bcd20
hjmoon <hjmoon222@gmail.com>
2016-05-27 17:39:29 +0900

    modify url

--- src/net/myegotrip/egotrip/FallbackDefaults.java
+++ src/net/myegotrip/egotrip/FallbackDefaults.java
@@ -1,7 +1,7 @@
 package net.myegotrip.egotrip;
 
 public interface FallbackDefaults {
-	public String LOCATION_UPLOAD_URL="http://upload.myegotrip.net/upload/postcoord.php";
-	public String LOCATION_VIEW_URL="http://upload.myegotrip.net/upload/view.php";
+	public String LOCATION_UPLOAD_URL="http://27.255.71.67:8080/test/postcoord.php";
+	public String LOCATION_VIEW_URL="http://27.255.71.67:8080/test/view.php";
 	public String DEFAULT_TRIP_NAME="default";
 }
