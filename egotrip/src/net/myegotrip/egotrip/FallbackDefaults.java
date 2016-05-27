a652835cecd45ee66ebcd43e03e17d3ebec28fc6
hjmoon <hjmoon222@gmail.com>
2016-05-27 17:18:38 +0900

    add egotrip

---
+++ egotrip/src/net/myegotrip/egotrip/FallbackDefaults.java
@@ -0,0 +1,7 @@
+package net.myegotrip.egotrip;
+
+public interface FallbackDefaults {
+	public String LOCATION_UPLOAD_URL="http://27.255.71.67:8080/test/postcoord.php";//"http://upload.myegotrip.net/upload/postcoord.php";
+	public String LOCATION_VIEW_URL="http://27.255.71.67:8080/test/view.php";
+	public String DEFAULT_TRIP_NAME="default";
+}
